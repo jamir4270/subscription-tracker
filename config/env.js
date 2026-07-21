169029dac9f8155c98022e4530ffa234de7b659e
jamir4270 <jamirandrade4270@gmail.com>
2026-07-21 12:26:33 +0800

    chore: setup project and environment

---
+++ config/env.js
@@ -0,0 +1,17 @@
+import { config } from "dotenv";
+
+config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+
+export const { 
+    PORT, 
+    SERVER_URL,
+    NODE_ENV, 
+    DB_URI, 
+    JWT_SECRET, 
+    JWT_EXPIRES_IN, 
+    ARCJET_KEY,
+    ARCJECT_ENV,
+    QSTASH_TOKEN,
+    QSTASH_URL,
+    EMAIL_PASSWORD,
+} = process.env;
