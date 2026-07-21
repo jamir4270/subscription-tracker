a11ed4df892f72091bcddf5a14eb5491b7e1822e
jamir4270 <jamirandrade4270@gmail.com>
2026-07-21 12:27:30 +0800

    feat: implement user routes and controllers

---
+++ routes/user.routes.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import { getUsers, getUser } from "../controllers/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
+import errorMiddleware from "../middlewares/error.middleware.js";
+
+const userRouter = Router();
+
+userRouter.get('/', getUsers);
+
+userRouter.get('/:id', authorize, errorMiddleware, getUser);
+
+userRouter.post('/', (req, res) => {
+    res.send({ title: "CREATE new user" });
+})
+
+userRouter.put('/:id', (req, res) => {
+    res.send({ title: "UPDATE user by id" });
+});
+
+userRouter.delete('/:id', (req, res) => {
+    res.send({ title: "DELETE user by id" });
+});
+
+export default userRouter;
