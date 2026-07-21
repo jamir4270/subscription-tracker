9a239525ee89f782b00e6fe5e97fa2a59905f49e
jamir4270 <jamirandrade4270@gmail.com>
2026-07-21 13:01:58 +0800

    fix: fix sequence of calls in app js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 
-import { PORT } from "./config/env.js";
+import { PORT, NODE_ENV } from "./config/env.js";
 
 import authRouter from './routes/auth.routes.js';
 import userRouter from './routes/user.routes.js';
@@ -15,26 +15,29 @@ import workflowRouter from './routes/workflow.routes.js';
 
 const app = express();
 
-app.use(errorMiddleware);
 app.use(arcjetMiddleware);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/users', userRouter);   
-app.use('/api/v1/subscriptions', subscriptionRouter);
-app.use('/api/v1/workflows', workflowRouter);
-
-app.listen(PORT, async () => {
-    console.log(`SubDub API is running on http://localhost:${PORT}`);
-
-    await connectToDatabase();
-});
-
 app.get("/", (req, res) => {
     res.send("Welcome to SubaDub API!");
 });
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use('/api/v1/workflows', workflowRouter);
+
+app.use(errorMiddleware);
+
+await connectToDatabase();
+
+if (NODE_ENV !== 'production') {
+    app.listen(PORT, async () => {
+        console.log(`SubDub API is running on http://localhost:${PORT}`);
+    });
+}
 
 export default app;
